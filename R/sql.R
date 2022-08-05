f0959c54ec53bd2bab63bdafb1ab6320dda96c0b
Konrad Siek <konrad.siek@gmail.com>
2022-08-05 13:55:09 +0200

    SQLite support.

--- R/sql.R
+++ R/sql.R
@@ -2,8 +2,8 @@
 # library(DBI)
 # library(RSQLite)
 
-sqlite_column_type <- function(table, column, ...)  {
-    connection <- DBI::dbConnect(drv = RSQLite::SQLite(), ...)
+sqlite_column_type <- function(db, table, column, ...)  {
+    connection <- do.call(DBI::dbConnect, c(drv = RSQLite::SQLite(), db, ...))
     result <- DBI::dbSendQuery(connection, paste0("PRAGMA table_info(", DBI::dbQuoteIdentifier(connection, table), ")"))
     columns <- DBI::dbFetch(result)
     DBI::dbClearResult(result)
@@ -24,8 +24,8 @@ sqlite_column_type <- function(table, column, ...)  {
     stop("Column \"", column, "\" from table \"", table,"\" has type ", one_column$type, " which cannot be represented as an R vector")
 }
 
-sqlite_column_length <- function(table, column, ...)  {
-    connection <- DBI::dbConnect(drv = RSQLite::SQLite(), ...)
+sqlite_column_length <- function(db, table, column, ...)  {
+    connection <- do.call(DBI::dbConnect, c(drv = RSQLite::SQLite(), db, ...))
     result <- DBI::dbSendQuery(connection, paste0("SELECT COUNT(*) FROM ", DBI::dbQuoteIdentifier(connection, table), ""))
     column_length <- DBI::dbFetch(result)
     DBI::dbClearResult(result)
@@ -40,33 +40,87 @@ sqlite_column_length <- function(table, column, ...)  {
     column_length$`COUNT(*)`
 }
 
-sqlite_select_values <- function(start, end, table, column, ...) {
-    connection <- DBI::dbConnect(drv = RSQLite::SQLite(), ...)
+# Start and end are zero-indexed
+sqlite_populate_vector <- function(db, start, end, table, column, constructor, converter, ...) {
 
-    DBI::dbSendQuery(connection, paste0(
+    # print(paste0("db:          ", db))
+    # print(paste0("start:       ", start))
+    # print(paste0("end:         ", end))
+    # print(paste0("table:       ", table))
+    # print(paste0("column:      ", column))
+    # print(paste0("constructor: ", constructor))
+    # print(paste0("converter:   ", converter))
+    # print(paste0("...:         ", list(...)))
+
+    connection <- do.call(DBI::dbConnect, c(drv = RSQLite::SQLite(), db, ...))
+
+    result <- DBI::dbSendQuery(connection, paste0(
         "SELECT", DBI::dbQuoteIdentifier(connection, column), "FROM (", 
-            "SELECT ROW_NUMBER() OVER(ORDER BY ROWID) __ufo_index, ", 
+            "SELECT ROW_NUMBER() OVER(ORDER BY ROWID) __ufo_index, ",   # ROWID is 1-indexed
                 DBI::dbQuoteIdentifier(connection, column), 
             " FROM ", DBI::dbQuoteIdentifier(connection, table), 
-        ") WHERE __ufo_index >= ", start, " AND  __ufo_index < ", end))   
-}
+        ") WHERE __ufo_index > ", start, " AND  __ufo_index <= ", end)) # conversion from 0-index to 1-index via these conditions
 
-sqlite_select_integer_values <- function(start, end, table, column, ...) {
-    result <- sqlite_select_values(start=start, end=end, table=table, column=column, ...)
-
-    output <- integer(end - start)
+    output <- constructor()
     cursor <- as.integer(0)
 
     while (!DBI::dbHasCompleted(result)) {
         data <- DBI::dbFetch(result)
         end_position <- cursor + as.integer(nrow(data))
-        output[cursor:end_position] <- as.integer(data[, column])
+        output[cursor:end_position] <- converter(data[, column])
     }
+
+    # print("output:")
+    # print(output)
 
     DBI::dbClearResult(result)
     output
 }
 
+sqlite_populate_integer <- function(db, start, end, table, column, ...) {
+    sqlite_populate_vector(
+        db=db, start=start, end=end, 
+        table=table, column=column, 
+        constructor=integer, converter=as.integer,
+        ...
+    )
+}
+
+sqlite_populate_character <- function(db, start, end, table, column, ...) {
+    sqlite_populate_vector(
+        db=db, start=start, end=end, 
+        table=table, column=column, 
+        constructor=character, converter=as.character,
+        ...
+    )
+}
+
+sqlite_populate_numeric <- function(db, start, end, table, column, ...) {
+    sqlite_populate_vector(
+        db=db, start=start, end=end, 
+        table=table, column=column, 
+        constructor=numeric, converter=as.numeric,
+        ...
+    )
+}
+
+sqlite_populate_raw <- function(db, start, end, table, column, ...) {
+    sqlite_populate_vector(
+        db=db, start=start, end=end, 
+        table=table, column=column, 
+        constructor=raw, converter=as.raw,
+        ...
+    )
+}
+
+sqlite_populate_logical <- function(db, start, end, table, column, ...) {
+    sqlite_populate_vector(
+        db=db, start=start, end=end, 
+        table=table, column=column, 
+        constructor=logical, converter=as.logical,
+        ...
+    )
+}
 
 #' Creates a UFO object of representing a column from an SQL database
 #' Uses seq.int internally to generate each fragment of the sequence.
@@ -82,38 +136,32 @@ sqlite_select_integer_values <- function(start, end, table, column, ...) {
 #' @return a ufo vector lazily populated with the values of the specified
 #'         column
 #' @export
-ufo_sql_db_column <- function(table, column, driver = "SQLite", chunk_length = 0, ...) {
+ufo_sql_db_column <- function(db, table, column, ..., driver = "SQLite") {
 
     column_type <- NULL
     column_length <- NULL
 
     if (driver == "SQLite" || driver == "SQLITE" || driver == "sqlite") {
-        column_type   <- sqlite_column_type(table = table, column = column, ...)
-        column_length <- sqlite_column_length(table = table, column = column, ...)
+        column_type   <- sqlite_column_type(db = db, table = table, column = column, ...)
+        column_length <- sqlite_column_length(db = db, table = table, column = column, ...)
     } else {
-        stop(paste0("Unsupported database driver: ", db_type, ". Use one of: SQLite"))
+        stop(paste0("Unsupported database driver: ", driver, ". Use one of: SQLite"))
     }
 
-    
+    populate <- if (column_type == "integer") sqlite_populate_integer
+        else if (column_type == "numeric") sqlite_populate_numeric
+        else if (column_type == "raw") sqlite_populate_raw
+        else if (column_type == "character") sqlite_populate_character
+        else if (column_type == "logical") sqlite_populate_logical
+        else { 
+            stop("Column \"", column, "\" from table \"", table,"\" has type ",type, " which cannot be represented as a UFO. It has to be one of: integer, numeric, raw, character, or logical.")
+        }
 
-    ufo_vector_constructor(mode = column_type, length = column_length, ...)
+    # populate(db=db, start=1, end=column_length + 1, table=table, column=column, ...)
+
+    ufo_vector_constructor(mode = column_type, length = column_length,
                populate = populate, 
-               #writeback = writeback,
-               #reset = reset, destroy = destroy,
-               #finalizer = finalizer, read_only = read_only,
-               chunk_length = chunk_length, ...)
+               db = db, table = table, column = column,
+               ...
     )
-
-
-    # ufo_integer_constructor(
-    #     length = (to - from) / by + 1,
-    #     populate = function(start, end, from, by, ...) {
-    #         as.numeric(seq.int(
-    #             from = from + (start - 1) * by,
-    #             to   = from + (end   - 1) * by,
-    #             by   = by
-    #         ))
-    #     },
-    #     ...
-    # )
 }
